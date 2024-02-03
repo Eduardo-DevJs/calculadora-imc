6f2cb315f356efec4fc2a28233350189ef6cff97
Eduardo-DevJs <dudev2001@gmail.com>
2024-02-03 01:27:52 -0300

    Feature / Criando o GridItem

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,4 +1,9 @@
 import { LevelsProps } from '../../Helpers/imc';
+import styles from "./GridItem.module.css"
+import { FaThumbsUp } from "react-icons/fa";
+import { FaThumbsDown } from "react-icons/fa";
+
+
 
 type Props = {
   item: LevelsProps;
@@ -6,8 +11,19 @@ type Props = {
 
 const GridItem = ({item}: Props) => {
   return (
-    <div>
-
+    <div className={styles.main} style={{backgroundColor: item.color}}>
+      <div className={styles.gridIcon}>
+        {item.icon === "up" && <FaThumbsUp size={30} color='#FFF'/>}
+        {item.icon === "down" && <FaThumbsDown size={30} color='#FFF'/>}
+      </div>
+      <div className={styles.gridTitle}>
+        {item.title}
+      </div>
+      <div className={styles.gridInfo}>
+        <>
+          IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+        </>
+      </div>
     </div>
   )
 };
