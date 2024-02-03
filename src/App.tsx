8b51eb89a83513cc8be64f575230591285d3a32d
Eduardo-DevJs <dudev2001@gmail.com>
2024-02-03 00:54:08 -0300

    Feature / Helpers (ajudadores)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
+import { levels, calulateImc} from "./Helpers/imc"
+
 
 const App = () => {
 
@@ -8,7 +10,7 @@ const App = () => {
 
   function calcularIMC(){
     if(campoPeso && campoAltura){
-      
+ 
     }else {
       alert("Digite todoso os campos")
     }
---
+++ src/Helpers/imc.ts
@@ -0,0 +1,48 @@
+export type LevelsProps = {
+  title: string;
+  color: string;
+  icon: "down" | 'up'
+  imc: number[]
+  youImc?: number
+}
+
+export const levels: LevelsProps[] = [
+  {
+    title: "Magreza",
+    color: "#96a3ab",
+    icon: "down",
+    imc: [0,18.5]
+  },
+  {
+    title: "Normal",
+    color: "#0ead69",
+    icon: "up",
+    imc: [18.6, 24.9]
+  },
+  {
+    title: "Sobrepeso",
+    color: "#e2b039",
+    icon: "down",
+    imc: [25,30]
+  },
+  {
+    title: "Sobrepeso",
+    color: "#c3423f",
+    icon: "down",
+    imc: [30.1,99]
+  }
+]
+
+export function calulateImc(altura: number, peso: number){ 
+  const imc = peso / (altura * altura)
+
+  for(let i in levels){
+    if(imc >= levels[i].imc[0] && imc <= levels[i].imc[1]){
+      levels[i].youImc = imc
+      return levels[i]
+    }
+
+
+    return null 
+  }
+}
