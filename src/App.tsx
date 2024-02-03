d2207a8633638cded98db9baf817bffed8ffddf2
Eduardo-DevJs <dudev2001@gmail.com>
2024-02-03 00:15:39 -0300

    feature / Criando a estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="">
+      <header>
+        <div className={styles.headerContainer}>
+          <div className={styles.headerLogo}>
+            <h2 className={styles.logo}>IMC</h2>
+            <span>Feito por Dudu Oliveira</span>
+          </div>
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          
+        </div>
+        <div className={styles.rightSide}>
 
-export default App
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
