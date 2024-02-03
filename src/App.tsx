df8215473544910c7820388f3fdf71ded12004cf
Eduardo-DevJs <dudev2001@gmail.com>
2024-02-03 00:30:23 -0300

    Feature / Criando o LeftSide

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 
 const App = () => {
+
+  const [campoAltura, setCampoAltura] = useState<number>(0)
+  const [campoPeso, setCampoPeso] = useState<number>(0)
+
+  function calcularIMC(){
+    if(campoPeso && campoAltura){
+      
+    }else {
+      alert("Digite todoso os campos")
+    }
+  }
+
   return (
     <div className="">
       <header>
@@ -13,7 +26,24 @@ const App = () => {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para Indice de Massa Corpórea, parâmetro adotado pela Organização Mundial da Saúde para Calcular o peso ideal de cada Pessoa</p>
+
+          <input 
+            type='number'
+            placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
+            value={campoAltura > 0 ? campoAltura : ""}
+            onChange={e => setCampoAltura(parseFloat(e.target.value))}
+          />
+
+          <input 
+            type='number'
+            placeholder='Digite o seu peso. Ex: 60.5 (em kg)'
+            value={campoPeso > 0 ? campoPeso : ""}
+            onChange={e => setCampoPeso(parseFloat(e.target.value))}
+          />
+
+          <button onClick={calcularIMC}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
 
